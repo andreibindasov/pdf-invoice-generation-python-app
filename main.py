26eb1124654b741407a4257ee6733bc31e1ec69f
andreibindasov <bindasov.andrei@gmail.com>
2023-06-20 13:41:07 -0600

    load excel files into dataframe / openpyxl package installed

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import pandas as pd
 from fpdf import FPDF
+import glob
 
+filepaths = glob.glob("invoices/*.xlsx")
+
+for fp in filepaths:
+    df = pd.read_excel(fp, sheet_name="Sheet 1")
+    print(df)
